883b0ad946673dd4f57965d0d675cddd6a04095f
cameronjamesw <>
2024-01-11 20:37:18 +0000

    add increment score and incremenet wrong answer functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,8 +42,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert('You got it right! :)');
+        incrementScore();
     } else {
         alert(`Unlucky, you answered ${userAnswer}, the correct answer was ${calculatedAnswer[0]}`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -64,13 +66,17 @@ function calculateCorrectAnswer() {
         throw `unimplemented operator ${operator}`;
     }
 }
-
+/**
+ * Gets the current score and adds one
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 }
 
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
