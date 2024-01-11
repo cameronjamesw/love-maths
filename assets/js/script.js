ac67fafbe791fe0fde8cbe910620603122d55252
cameronjamesw <>
2024-01-11 14:50:12 +0000

    add empty functions to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function runGame();
+
+function checkAnswer();
+
+function calculateCorrectAnwer();
+
+function incrementScore();
+
+function incrementWrongAnswer();
+
+function displayAdditionQuestion();
+
+function displaySubtractQuestion();
+
+function displayMultiplyQuestion();
